ca7bec0f8f7da930e2de550f8f39a2b0cab9ed6f
simon <simon2020291@gmail.com>
2021-06-03 14:25:47 +0800

    [add] lambda of topic_extractor transcribe record and extract a topic

--- topic_extractor/lambda_function.py
+++ topic_extractor/lambda_function.py
@@ -1,2 +1,22 @@
+from boto3 import client
+from .transcribe_handler import TranscribeHandler
+from .extractor import Extractor
+
+
+
+transcribe = client('transcribe', region_name='us-east-1')
+transcribe_handler = TranscribeHandler(transcribe)
+extractor = Extractor()
+
 def lambda_handler(event, context):
+    for record in event['Records']:
+        s3_event = record['s3']
+        print(s3_event)
+
+        bucket_name = s3_event['bucket']['name']
+        key = s3_event['object']['key'].replace('%40', '@')
+
+        texts = transcribe_handler.record_to_texts(bucket_name, key)
+        topic = extractor.extract_topic(texts)
+        print(f'Topic: {topic}')
     return
