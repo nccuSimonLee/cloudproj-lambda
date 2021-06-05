09f1f45c4c7fb0415a958bde70be267621ce418c
simon <simon2020291@gmail.com>
2021-06-05 18:39:10 +0800

    [add] topic_extractor wiill write data to ddb table

--- topic_extractor/lambda_function.py
+++ topic_extractor/lambda_function.py
@@ -1,12 +1,17 @@
 from boto3 import client
-from .transcribe_handler import TranscribeHandler
-from .extractor import Extractor
+from datetime import datetime
+import pytz
+import os
+import uuid
+from transcribe_handler import TranscribeHandler
+from extractor import Extractor
 
 
 
 transcribe = client('transcribe', region_name='us-east-1')
 transcribe_handler = TranscribeHandler(transcribe)
 extractor = Extractor()
+ddb = client('dynamodb', region_name='us-east-1')
 
 def lambda_handler(event, context):
     for record in event['Records']:
@@ -15,8 +20,30 @@ def lambda_handler(event, context):
 
         bucket_name = s3_event['bucket']['name']
         key = s3_event['object']['key'].replace('%40', '@')
-
         texts = transcribe_handler.record_to_texts(bucket_name, key)
         topic = extractor.extract_topic(texts)
         print(f'Topic: {topic}')
+
+        topic_id = uuid.uuid4().hex
+        event_datetime = convert_event_timezone(record['eventTime'])
+        event_date, event_time = event_datetime.split(' ')
+        response = ddb.put_item(
+            TableName=os.environ['TABLE_NAME'],
+            Item={
+                'topic_id': {'S': topic_id},
+                'date': {'S': event_date},
+                'time': {'S': event_time},
+                'topic': {'S': topic}
+            }
+        )
+
     return
+
+def convert_event_timezone(dt_str):
+    orig_dt = datetime.strptime(
+        dt_str[:-5],
+        '%Y-%m-%dT%H:%M:%S'
+    ).replace(tzinfo=pytz.UTC)
+    taipei_dt = orig_dt.astimezone(pytz.timezone('Asia/Taipei'))
+    taipei_dt_str = taipei_dt.strftime('%Y-%m-%d %H:%M:%S')
+    return taipei_dt_str
