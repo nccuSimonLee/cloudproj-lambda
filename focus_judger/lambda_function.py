9be38e6bf2d4835a17484627ca561fc736e8d231
simon <simon2020291@gmail.com>
2021-06-09 08:40:57 +0800

    [refactor] make the code for ddb in lambda_function of focus_judger into a function

--- focus_judger/lambda_function.py
+++ focus_judger/lambda_function.py
@@ -18,20 +18,13 @@ def lambda_handler(event, context):
         # determine if there are any faces in the photo
         has_face = check_s3_event_has_face(record['s3'])
         
-        # write the result to the dynamoDB table
+        # create the attributes of the table item
         username = record['s3']['object']['key'].split('/')[0].replace('%40', '@')
         event_datetime = convert_event_timezone(record['eventTime'])
         event_date, event_time = event_datetime.split(' ')
-        response = ddb.put_item(
-            TableName=os.environ['TABLE_NAME'],
-            Item={
-                'username': {'S': username},
-                'date': {'S': event_date},
-                'time': {'S': event_time},
-                'focus': {'S': str(has_face)}
-            }
-        )
         print(f'{username} - {event_date} {event_time} - focus: {has_face}')
+
+        response = write_to_ddb(username, event_date, event_time, has_face)
     return
 
 def check_s3_event_has_face(s3_event):
@@ -57,3 +50,15 @@ def convert_event_timezone(dt_str):
     taipei_dt = orig_dt.astimezone(pytz.timezone('Asia/Taipei'))
     taipei_dt_str = taipei_dt.strftime('%Y-%m-%d %H:%M:%S')
     return taipei_dt_str
+
+def write_to_ddb(username, event_date, event_time, has_face):
+    response = ddb.put_item(
+        TableName=os.environ['TABLE_NAME'],
+        Item={
+            'username': {'S': username},
+            'date': {'S': event_date},
+            'time': {'S': event_time},
+            'focus': {'N': str(int(has_face))}
+        }
+    )
+    return response
