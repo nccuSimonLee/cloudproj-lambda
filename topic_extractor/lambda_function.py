912001a27d9e0790747dc314d03b8855a64834a2
simon <simon2020291@gmail.com>
2021-06-15 08:45:18 +0800

    refactor: pass attributes of topic with topic_data

--- topic_extractor/lambda_function.py
+++ topic_extractor/lambda_function.py
@@ -20,30 +20,42 @@ def lambda_handler(event, context):
         topic = extractor.extract_topic(body['texts'])
         print(f'Topic: {topic}')
 
-        username = body['username']
+        topic_data = create_topic_data(topic, body)
 
-        response = write_to_ddb(username, topic, body)
+        response = write_to_ddb(topic_data)
         print(response)
 
-        publish_to_line_group(username, topic)
+        publish_to_line_group(topic_data)
 
     return
 
-def write_to_ddb(username, topic, body):
+def create_topic_data(topic, body):
     topic_id = uuid.uuid4().hex
+    topic_data = {
+        'topic_id': topic_id,
+        'username': body['username'],
+        'date': body['date'],
+        'time': body['time'],
+        'topic': topic
+    }
+    return topic_data
+
+def write_to_ddb(topic_data):
+    item = {
+        'topic_id': {'S': topic_data['topic_id']},
+        'username': {'S': topic_data['username']},
+        'date': {'S': topic_data['date']},
+        'time': {'S': topic_data['time']},
+        'topic': {'S': topic_data['topic']}
+    }
     response = ddb.put_item(
         TableName=os.environ['TABLE_NAME'],
-        Item={
-            'topic_id': {'S': topic_id},
-            'username': {'S': username},
-            'date': {'S': body['date']},
-            'time': {'S': body['time']},
-            'topic': {'S': topic}
-        }
+        Item=item
     )
     return response
 
-def publish_to_line_group(username, topic):
+def publish_to_line_group(topic_data):
+    username, topic = topic_data['username'], topic_data['topic']
     line_bot_api.push_message(
         os.environ['GROUP_ID'],
         TextSendMessage(text=f'@{username} 剛剛發佈了新問題，快來看看吧！\n{topic}')
